a15ab11cf55858eb50a18dfae5d8aee9c8b468aa
Nicholas Tomlin <nicholas.tomlin@ode.ri.gov>
2016-08-29 16:02:48 -0400

    working python script

--- access.py
+++ access.py
@@ -1,9 +1,14 @@
+# Code from http://stackoverflow.com/questions/25820698/how-do-i-import-an-accdb-file-into-python-and-use-the-data.
+# Installed Microsoft Access Database Engine 2010 (linked above).
+
 import pypyodbc
 pypyodbc.lowercase = False
 conn = pypyodbc.connect(
     r"Driver={Microsoft Access Driver (*.mdb, *.accdb)};" +
-    r"Dbq=C:\Users\Nicholas.Tomlin\Desktop\TIP17-25.accdb;")
+    # path to TIP17-25 below should be reconfigured as needed
+    r"Dbq=C:\Users\Nicholas.Tomlin\Documents\GitHub\TIP\TIP17-25.accdb;")
 cur = conn.cursor()
+# change the SQL query below to alter the data that can be accessed
 cur.execute("SELECT ID, TIPID, ProjectName FROM AllProjects");
 while True:
     row = cur.fetchone()
