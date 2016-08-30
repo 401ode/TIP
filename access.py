a3e609dfc15fc0a11ce02d0f4fdde8bbb5d8561d
Nicholas Tomlin <nicholas.tomlin@ode.ri.gov>
2016-08-30 09:45:45 -0400

    pandas integration

--- access.py
+++ access.py
@@ -2,18 +2,26 @@
 # Installed Microsoft Access Database Engine 2010 (linked above).
 
 import pypyodbc
+import pandas as pd
+import numpy as np
+
 pypyodbc.lowercase = False
 conn = pypyodbc.connect(
     r"Driver={Microsoft Access Driver (*.mdb, *.accdb)};" +
     # path to TIP17-25 below should be reconfigured as needed
     r"Dbq=C:\Users\Nicholas.Tomlin\Documents\GitHub\TIP\TIP17-25.accdb;")
-cur = conn.cursor()
+
+# cur = conn.cursor()
 # change the SQL query below to alter the data that can be accessed
-cur.execute("SELECT ID, TIPID, ProjectName FROM AllProjects");
-while True:
-    row = cur.fetchone()
-    if row is None:
-        break
-    print (u"{2} has ID {0} and TIP ID {1}.".format(row.get("ID"), row.get("TIPID"), row.get("ProjectName")))
-cur.close()
+# cur.execute("SELECT (SELECT * FROM FY17) AS f17, (SELECT * FROM FY18) AS f18, (SELECT * FROM FY19) AS f19, (SELECT * FROM FY20) AS f20");
+
+# while True:
+#    row = cur.fetchone()
+#    if row is None:
+#        break
+#    print (u"{2} has ID {0} and TIP ID {1}.".format(row.get("ID"), row.get("TIPID"), row.get("ProjectName")))
+
+df = pd.read_sql("SELECT ID, TIPID, ProjectName FROM AllProjects", conn)
+print(df.head());
+# cur.close()
 conn.close()
