829aa98f4ec18ace2560ef83eb457c15aa66f65e
Nicholas Tomlin <nicholas.tomlin@ode.ri.gov>
2016-09-01 17:18:48 -0400

    added RIPTA Funding info
    still need to match munis by TIPID

--- ColumnHeaders.py
+++ ColumnHeaders.py
@@ -9,8 +9,8 @@ conn = pypyodbc.connect(
     r"Driver={Microsoft Access Driver (*.mdb, *.accdb)};" +
     r"Dbq=C:\Users\Nicholas.Tomlin\Documents\GitHub\TIP\TIP17-25.accdb;")
 
-df1 = pd.read_sql("SELECT * FROM FY16", conn);
-df2 = pd.read_sql("SELECT * FROM FY16_BG", conn);
+df1 = pd.read_sql("SELECT * FROM RIPTAFunding", conn);
+df2 = pd.read_sql("SELECT * FROM FY16_RIPTAFunding", conn);
 
 print(str(list(df1.columns.values)).replace("'",""));
 print(str(list(df2.columns.values)).replace("'",""));
