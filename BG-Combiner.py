6db5d213f6b282905e1a0fd72841fe7d8fadc474
Nicholas Tomlin <nicholas.tomlin@ode.ri.gov>
2016-09-01 12:52:15 -0400

    added municipalities by TIPID

--- BG-Combiner.py
+++ BG-Combiner.py
@@ -12,30 +12,37 @@ conn = pypyodbc.connect(
     # path to TIP17-25 below should be reconfigured as needed
     r"Dbq=C:\Users\Nicholas.Tomlin\Documents\GitHub\TIP\TIP17-25.accdb;")
 
+munis = pd.read_sql("SELECT TIPID, Municipalities FROM IDmunicipalities", conn);
+
 df17 = pd.read_sql("SELECT * FROM FY17", conn);
 bg17 = pd.read_sql("SELECT * FROM FY17_BG", conn);
 df1 = pd.concat([df17, bg17]);
-df1.to_csv(r'C:\Users\Nicholas.Tomlin\Documents\GitHub\TIP\CSVs\FY17.csv', index=False);
+df = pd.merge(munis, df1, how='outer', on='TIPID');
+df.to_csv(r'C:\Users\Nicholas.Tomlin\Documents\GitHub\TIP\CSVs\FY17.csv', index=False);
 
 df18 = pd.read_sql("SELECT *, FY18Allocation AS Allocation FROM FY18", conn);
 bg18 = pd.read_sql("SELECT *, FY18allocation AS Allocation FROM FY18_BG", conn);
 df2 = pd.concat([df18, bg18]);
-df2.to_csv(r'C:\Users\Nicholas.Tomlin\Documents\GitHub\TIP\CSVs\FY18.csv', index=False);
+df = pd.merge(munis, df2, how='outer', on='TIPID');
+df.to_csv(r'C:\Users\Nicholas.Tomlin\Documents\GitHub\TIP\CSVs\FY18.csv', index=False);
 
 df19 = pd.read_sql("SELECT * FROM FY19", conn);
 bg19 = pd.read_sql("SELECT * FROM FY19_BG", conn);
 df3 = pd.concat([df19, bg19]);
-df3.to_csv(r'C:\Users\Nicholas.Tomlin\Documents\GitHub\TIP\CSVs\FY19.csv', index=False);
+df = pd.merge(munis, df3, how='outer', on='TIPID');
+df.to_csv(r'C:\Users\Nicholas.Tomlin\Documents\GitHub\TIP\CSVs\FY19.csv', index=False);
 
 df20 = pd.read_sql("SELECT * FROM FY20", conn);
 bg20 = pd.read_sql("SELECT * FROM FY20_BG", conn);
 df4 = pd.concat([df20, bg20]);
-df4.to_csv(r'C:\Users\Nicholas.Tomlin\Documents\GitHub\TIP\CSVs\FY20.csv', index=False);
+df = pd.merge(munis, df4, how='outer', on='TIPID');
+df.to_csv(r'C:\Users\Nicholas.Tomlin\Documents\GitHub\TIP\CSVs\FY20.csv', index=False);
 
 df21_25 = pd.read_sql("SELECT * FROM [FY21-25lump]", conn);
 bg21_25 = pd.read_sql("SELECT * FROM [FY21-25_BG]", conn);
 df5 = pd.concat([df21_25, bg21_25]);
-df5.to_csv(r'C:\Users\Nicholas.Tomlin\Documents\GitHub\TIP\CSVs\FY21-25.csv', index=False);
+df = pd.merge(munis, df5, how='outer', on='TIPID');
+df.to_csv(r'C:\Users\Nicholas.Tomlin\Documents\GitHub\TIP\CSVs\FY21-25.csv', index=False);
 
 # cur.close()
 conn.close()
